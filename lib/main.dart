c92e4abec5ac93b3b6399e2d72155eddd10e3d22
Ioannis Maras <maras2002@gmail.com>
2022-10-09 22:16:54 +0300

    some work on stockfish gui

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chess_board/flutter_chess_board.dart' as csb;
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:chess/chess.dart' as chess_lib;
 import 'package:window_manager/window_manager.dart';
@@ -70,10 +71,10 @@ class MyAppState extends State<MyApp> with WindowListener {
     setState(() {
       _stockfishOutputText += "$output\n";
     });
-    if (output.startsWith('bestmove')) {
+    if (output.contains('currmove')) {
       final parts = output.split(' ');
       setState(() {
-        _nextMove = parts[1];
+        _nextMove = parts[4];
       });
     }
   }
@@ -109,7 +110,7 @@ class MyAppState extends State<MyApp> with WindowListener {
       _stockfishOutputText = '';
     });
     _stockfish.stdin = 'position fen ${_fenController.text.trim()}';
-    _stockfish.stdin = 'go movetime ${_timeMs.toInt()}';
+    _stockfish.stdin = 'go infinite';
   }
 
   void _stopStockfish() async {
@@ -147,7 +148,7 @@ class MyAppState extends State<MyApp> with WindowListener {
   }
 
   Icon _getStockfishStatusIcon() {
-    Color color;
+    Color color = Colors.black;
     switch (_stockfish.state.value) {
       case StockfishState.ready:
         color = Colors.green;
@@ -162,82 +163,157 @@ class MyAppState extends State<MyApp> with WindowListener {
     return Icon(MdiIcons.circle, color: color);
   }
 
+  csb.ChessBoardController controller = csb.ChessBoardController();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: const Text("Stockfish Chess Engine example"),
+          title: const Text('Chess Demo'),
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
+        body: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: csb.ChessBoard(
+                  controller: controller,
+                  boardColor: csb.BoardColor.orange,
+                  arrows: [
+                    csb.BoardArrow(
+                      from: 'd2',
+                      to: 'd4',
+                      color: Colors.red.withOpacity(0.5),
+                    ),
+                  ],
+                  boardOrientation: csb.PlayerColor.white,
+                ),
+              ),
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                TextField(
-                  controller: _fenController,
-                  decoration: const InputDecoration(
-                    hintText: 'Position FEN value',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
+                _getStockfishStatusIcon(),
                 ElevatedButton(
-                  onPressed: _pasteFen,
-                  child: const Text('Paste FEN'),
+                  onPressed: _startStockfishIfNecessary,
+                  child: const Text('Start Stockfish'),
                 ),
-                Slider(
-                  value: _timeMs,
-                  onChanged: _updateThinkingTime,
-                  min: 500,
-                  max: 3000,
-                ),
-                Text('Thinking time : ${_timeMs.toInt()} millis'),
                 ElevatedButton(
                   onPressed: _computeNextMove,
                   child: const Text('Search next move'),
                 ),
-                Text('Best move: $_nextMove'),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    _getStockfishStatusIcon(),
-                    ElevatedButton(
-                      onPressed: _startStockfishIfNecessary,
-                      child: const Text('Start Stockfish'),
-                    ),
-                    ElevatedButton(
-                      onPressed: _stopStockfish,
-                      child: const Text('Stop Stockfish'),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    width: 850.0,
-                    height: 300.0,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        width: 2.0,
-                      ),
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(8.0),
-                      ),
-                    ),
-                    child: SingleChildScrollView(
-                      child: Text(
-                        _stockfishOutputText,
-                      ),
-                    ),
-                  ),
-                ),
               ],
             ),
-          ),
+            Expanded(
+              child: ValueListenableBuilder<csb.Chess>(
+                valueListenable: controller,
+                builder: (context, game, _) {
+                  _fenController.text = controller.getFen();
+                  return Text(_nextMove);
+                  // return Padding(
+                  //   padding: const EdgeInsets.all(8.0),
+                  //   child: Container(
+                  //     width: 850.0,
+                  //     height: 300.0,
+                  //     decoration: BoxDecoration(
+                  //       border: Border.all(
+                  //         width: 2.0,
+                  //       ),
+                  //       borderRadius: const BorderRadius.all(
+                  //         Radius.circular(8.0),
+                  //       ),
+                  //     ),
+                  //     child: SingleChildScrollView(
+                  //       child: Text(
+                  //         _stockfishOutputText,
+                  //       ),
+                  //     ),
+                  //   ),
+                  // );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: const Text("Stockfish Chess Engine example"),
+    //   ),
+    //   body: SingleChildScrollView(
+    //     child: Column(
+    //       children: [
+    //       ],
+    //     ),
+    //     // child: Padding(
+    //     //   padding: const EdgeInsets.all(8.0),
+    //     //   child: Column(
+    //     //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //     //     crossAxisAlignment: CrossAxisAlignment.center,
+    //     //     children: [
+    //     //       // TextField(
+    //     //       //   controller: _fenController,
+    //     //       //   decoration: const InputDecoration(
+    //     //       //     hintText: 'Position FEN value',
+    //     //       //     border: OutlineInputBorder(),
+    //     //       //   ),
+    //     //       // ),
+    //     //       // ElevatedButton(
+    //     //       //   onPressed: _pasteFen,
+    //     //       //   child: const Text('Paste FEN'),
+    //     //       // ),
+    //     //       // Slider(
+    //     //       //   value: _timeMs,
+    //     //       //   onChanged: _updateThinkingTime,
+    //     //       //   min: 500,
+    //     //       //   max: 3000,
+    //     //       // ),
+    //     //       // Text('Thinking time : ${_timeMs.toInt()} millis'),
+    //     //       // ElevatedButton(
+    //     //       //   onPressed: _computeNextMove,
+    //     //       //   child: const Text('Search next move'),
+    //     //       // ),
+    //     //       // Text('Best move: $_nextMove'),
+    //     //       // Row(
+    //     //       //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //     //       //   children: [
+    //     //       //     _getStockfishStatusIcon(),
+    //     //       //     ElevatedButton(
+    //     //       //       onPressed: _startStockfishIfNecessary,
+    //     //       //       child: const Text('Start Stockfish'),
+    //     //       //     ),
+    //     //       //     ElevatedButton(
+    //     //       //       onPressed: _stopStockfish,
+    //     //       //       child: const Text('Stop Stockfish'),
+    //     //       //     ),
+    //     //       //   ],
+    //     //       // ),
+    //     //       // Padding(
+    //     //       //   padding: const EdgeInsets.all(8.0),
+    //     //       //   child: Container(
+    //     //       //     width: 850.0,
+    //     //       //     height: 300.0,
+    //     //       //     decoration: BoxDecoration(
+    //     //       //       border: Border.all(
+    //     //       //         width: 2.0,
+    //     //       //       ),
+    //     //       //       borderRadius: const BorderRadius.all(
+    //     //       //         Radius.circular(8.0),
+    //     //       //       ),
+    //     //       //     ),
+    //     //       //     child: SingleChildScrollView(
+    //     //       //       child: Text(
+    //     //       //         _stockfishOutputText,
+    //     //       //       ),
+    //     //       //     ),
+    //     //       //   ),
+    //     //       // ),
+
+    //     //     ],
+    //     //   ),
+    //     // ),
+    //   ),
+    // );
   }
 }
